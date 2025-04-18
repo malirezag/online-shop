e246decba8ffb69e7879a4a5cf271175af888b2c
alireza <malirezag@gmail.com>
2025-04-19 00:48:14 +0330

    fix bug

--- src/ui/Brands.tsx
+++ src/ui/Brands.tsx
@@ -1,11 +1,11 @@
 function Brands() {
   return (
     <div className="bg-black w-full flex justify-around flex-wrap gap-6 px-4 py-7 ">
-      <img src="public/images/logo (2).png" alt="" />
-      <img src="public/images/logo (1).png" alt="" />
-      <img src="public/images/logo (5).png" alt="" />
-      <img src="public/images/logo (3).png" alt="" />
-      <img src="public/images/logo (4).png" alt="" />
+      <img src="/images/logo (2).png" alt="" />
+      <img src="/images/logo (1).png" alt="" />
+      <img src="/images/logo (5).png" alt="" />
+      <img src="/images/logo (3).png" alt="" />
+      <img src="/images/logo (4).png" alt="" />
     </div>
   );
 }
