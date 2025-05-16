1b604972e5ed7d4324232f0e6d690ddad1f619d8
alireza <malirezag@gmail.com>
2025-05-17 01:36:54 +0330

    a

--- src/components/orders/CartOrders.tsx
+++ src/components/orders/CartOrders.tsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import Button from "../../ui/Button";
 import Order from "../../ui/Order";
 import Spinner from "../../ui/Spinner";
 import Title from "../../ui/Title";
